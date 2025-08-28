a2c667b973d59e91aacb3103af3cb01b4e2d30ca
Rado Hidayatulloh <acountgithub20@gmail.com>
2025-08-28 10:20:57 +0700

    server

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ className, name, type }) {
-  return (
-    <button className={className} type={type}>
-      {name}
-    </button>
-  );
-}
--- src/service/authService.js
+++
@@ -1,8 +0,0 @@
-export async function RegisterPost(email, username, password) {
-  const res = await fetch("/api/v1/Register", {
-    method: "POST",
-    headers: { "Content-Type": "applicatio/json" },
-    body: JSON.stringify({ email, username, password }),
-  });
-  return res.json();
-}
